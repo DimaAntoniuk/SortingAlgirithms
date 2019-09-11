04f4db04720749c41e694b4ffefc84f2c60b17e3
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-09-11 10:20:29 +0300

    added counter to merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,7 +1,11 @@
+import time
+
 def merge(A, B):
     out = []
     a = b = 0
+    global comp
     while a < len(A) and b < len(B):
+        comp += 1
         if A[a] < B[b]:
             out.append(A[a])
             a += 1
@@ -9,6 +13,7 @@ def merge(A, B):
             out.append(B[b])
             b += 1
     out += A[a:] + B[b:]
+    comp += len(A)-a + len(B)-b
     return out
 
 def merge_sort(A):
@@ -18,13 +23,15 @@ def merge_sort(A):
     return merge(merge_sort(A[:mid]), merge_sort(A[mid:]))
 
 a = []
-
+comp = 0
 with open('input.txt', 'r') as input:
     line = input.read().split()
     for i in range(len(line)):
         a.append(int(line[i]))
-
+start = time.time()
 a = merge_sort(a)
-
+finish = time.time()
 with open('output.txt', 'w+') as output:
     output.write(str(a))
+    output.write('\n')
+    output.write(str(comp))
